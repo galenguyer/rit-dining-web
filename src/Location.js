5c6ff5111d2a17a477772aff804e05f32505ca48
Galen Guyer <galen@galenguyer.com>
2020-10-31 23:19:05 -0400

    add support for til_close to locations

--- src/Location.js
+++ src/Location.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./Location.css";
 
 const Location = (props) => {
-    return <div className={"Location " + (props.data.open ? "Location-Open" : "Location-Closed")}>{props.name}</div>;
+    if (props.data.til_close) {
+        return (
+            <div className={"Location " + (props.data.open ? "Location-Open" : "Location-Closed")}>
+                <span>{props.name}</span>
+                <br />
+                <span>{props.data.til_close} 'til close</span>
+            </div>
+        );
+    } else {
+        return (
+            <div className={"Location " + (props.data.open ? "Location-Open" : "Location-Closed")}>
+                <span>{props.name}</span>
+            </div>
+        );
+    }
 };
 
 export default Location;
